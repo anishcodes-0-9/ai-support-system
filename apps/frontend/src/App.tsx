3a6da4d151fa69bd61a6824e994d89c12c1c2ef5
anish-codes <anishkrishnan72@gmail.com>
2026-02-17 14:13:34 +0530

    separated frontend app and connect to backend API

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,14 +1,5 @@
-//import { useState } from "react";
-//import reactLogo from "./assets/react.svg";
-//import viteLogo from "/vite.svg";
-import "./App.css";
+import Chat from "./components/Chat";
 
 export default function App() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-black text-white text-2xl">
-      Tailwind Working
-    </div>
-  );
+  return <Chat />;
 }
-
-//export default App;
